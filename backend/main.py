ba89982ba44872cbb9ae556d76f30f86e66f14fb
Sidharth A <sidharth.a3721@gmail.com>
2026-03-08 16:45:10 +0530

    Added cheapest platform detection

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow frontend requests
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 products = {
     "milk": {"blinkit": 68, "zepto": 65},
     "eggs": {"blinkit": 90, "zepto": 88},
-    "bread": {"blinkit": 45, "zepto": 42}
+    "bread": {"blinkit": 45, "zepto": 42},
 }
 
 @app.get("/")
